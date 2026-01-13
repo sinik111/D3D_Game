8d7ed736e16b4be62580990a830219d134b9c172
hth-hiro <hth0114@naver.com>
2026-01-13 15:36:42 +0900

    feat | RectTransform 내부 구현 추가중

--- Engine/Framework/Object/Component/RectTransform.h
+++ Engine/Framework/Object/Component/RectTransform.h
@@ -4,10 +4,41 @@
 
 namespace engine
 {
+	struct UIRect
+	{
+		float x = 0.0f;
+		float y = 0.0f;
+		float w = 0.0f;
+		float h = 0.0f;
+
+		Vector2 Pos() const { return { x, y }; }
+		Vector2 Size() const { return { w, h }; }
+	};
+
 	class RectTransform :
 		public Transform
 	{
 	private:
+		Vector2 m_anchoredPosition{ 0.0f, 0.0f };
+		
+		float m_width = 100.0f;
+		float m_height = 100.0f;
+
+		Vector2 m_pivot{ 0.5f, 0.5f };
+
+		Vector2 m_anchorMin{ 0.5f, 0.5f };
+		Vector2 m_anchorMax{ 0.5f, 0.5f };
+
+		UIRect m_worldRect{};
+		bool m_uiDirty = true;
+
+	public:
+		RectTransform() = default;
+		~RectTransform() override = default;
+
+	public:
+		const Vector2& GetAnchoredPosition() const { return m_anchoredPosition; }
+		//const Vector2& GetSizeDelta() const { return m_sizeDelta; }
 
 	};
 
